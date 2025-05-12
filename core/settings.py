8e0da7875ed47923962ead87bb7bbb0ea4a258c2
VoidSentrycmd <cliquergreedcrew@gmail.com>
2025-05-12 19:18:29 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sxkj_i2xm7d7t)y95%v45y2h(xr%&m-@lz&umqqncl5jf*@yr5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [mental-forum.onrender.com]
 
 
 # Application definition
